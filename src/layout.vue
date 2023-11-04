5666fcc1c96cb16982b2e209f7eb6a68cee1847c
devrik99 <devrikoficial@gmail.com>
2023-11-04 14:59:52 -0500

    Se termimo la primera version de la vista de productos

--- src/layout.vue
+++ src/layout.vue
@@ -1,6 +1,8 @@
 <template>
   <Header />
-  <router-view></router-view>
+  <div class="container mx-auto my-5">
+    <router-view></router-view>
+  </div>
 </template>
 <script setup>
 import Header from "./components/Header.vue";
