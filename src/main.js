b6ebf20c4c61a6399b8c7e7a3907f3e244df5540
devrik99 <devrikoficial@gmail.com>
2023-11-04 14:04:47 -0500

    Se instalo box icons

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import "./assets/css/main.css";
 import "primevue/resources/themes/lara-light-teal/theme.css";
+import "boxicons/css/boxicons.min.css";
 
 import App from "./App.vue";
 const app = createApp(App);
